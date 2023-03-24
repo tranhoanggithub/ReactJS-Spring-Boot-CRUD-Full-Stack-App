47fe46397962f1df924376e7e04fcd6591337f48
tranhoanggithub <thoang.19i@cit.udn.vn>
2023-03-24 10:50:34 +0700

    add creact Employee

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -8,5 +8,9 @@ class EmloyeeService {
         return axios.get(EMPLOYEE_API_BASE_URL)
     }
 
+    createEmployee(employee) {
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
+
 }
 export default new EmloyeeService();
