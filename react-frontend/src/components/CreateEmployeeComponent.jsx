ecebd0798bd417b61b04364d87c63f26a8ab5887
tranhoanggithub <thoang.19i@cit.udn.vn>
2023-03-27 17:03:27 +0700

    push code AddandUpdateEmployee

--- react-frontend/src/components/CreateEmployeeComponent.jsx
+++ react-frontend/src/components/CreateEmployeeComponent.jsx
@@ -5,6 +5,7 @@ export default class CreateEmployeeComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            id: this.props.match.params.id,
             firstName: '',
             lastName: '',
             emailId: ''
@@ -12,17 +13,41 @@ export default class CreateEmployeeComponent extends Component {
         this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
         this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
         this.changeEmailHandler = this.changeEmailHandler.bind(this);
-        this.saveEmployee = this.saveEmployee.bind(this);
+        this.saveOrUpdateEmployee = this.saveOrUpdateEmployee.bind(this);
+    }
+    componentDidMount() {
+        if (this.state.id === '_add') {
+            return;
+        } else {
+            EmployeeService.getEmployeesById(this.state.id).then((res) => {
+                let employee = res.data;
+                this.setState({
+                    firstName: employee.firstName,
+                    lastName: employee.lastName,
+                    emailId: employee.emailId
+                });
+            });
+
+        }
+
+
     }
 
-    saveEmployee = (event) => {
-        // event.prevenDefault();
+    saveOrUpdateEmployee = (e) => {
+        e.prevenDefault();
         let employee = { firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId };
-        console.log('employ=>' + JSON.stringify(employee))
+        console.log('employ=>' + JSON.stringify(employee));
+        if (this.state.id === '_add') {
+            EmployeeService.createEmployee(employee).then(res => {
+                this.props.history.push('/employees')
+            })
+        } else {
+            EmployeeService.updateEmployee(employee, this.state.id).then(res => {
+                this.props.history.push('/employees');
+            })
+        }
 
-        EmployeeService.createEmployee(employee).then(res => {
-            this.props.history.push('/employees')
-        })
+
     }
     changeFirstNameHandler(event) {
         this.setState({ firstName: event.target.value });
@@ -38,13 +63,23 @@ export default class CreateEmployeeComponent extends Component {
         this.props.history.push('/employees');
     }
 
+    getTitle() {
+        if (this.state.id === '_add') {
+            return <h3 className="text-center">Add Employee</h3>
+        } else {
+            return <h3 className='text-center'>Update Employee</h3>
+        }
+    }
+
     render() {
         return (
             <div>
                 <div className="container">
                     <div className="row">
                         <div className='card col-md-6 offset-md-3 offset-md-3 offset-md-3'>
-                            <h3 className='text-center'>Add Employee</h3>
+                            {
+                                this.getTitle()
+                            }
                             <div className="card-body">
                                 <form>
                                     <div className="form-group">
@@ -62,7 +97,7 @@ export default class CreateEmployeeComponent extends Component {
                                         <input placeholder="Email Address" name="emailId" className="form-control"
                                             value={this.state.emailId} onChange={this.changeEmailHandler} />
                                     </div>
-                                    <button className="btn btn-success" onClick={this.saveEmployee}>Save</button>
+                                    <button className="btn btn-success" onClick={this.saveOrUpdateEmployee}>Save</button>
                                     <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{ marginLeft: "10px" }}>Cancel</button>
 
                                 </form>
